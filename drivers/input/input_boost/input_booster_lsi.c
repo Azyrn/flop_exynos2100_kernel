7897b00fca79495a935562c04548c603b452afc7
skeler <salahlsxusa@gmail.com>
2026-07-27 23:22:06 +0100

    input: restore device-tree-native touch boosting
    Signed-off-by: skeler <salahlsxusa@gmail.com>

--- drivers/input/input_boost/input_booster_lsi.c
+++ drivers/input/input_boost/input_booster_lsi.c
@@ -7,13 +7,10 @@
 #include <soc/samsung/exynos_pm_qos.h>
 #include <soc/samsung/exynos-ufcc.h>
 
-#define TOUCH_DISP_BOOST_KHZ		(663 * 1000)
-
 struct exynos_pm_qos_request cluster0_qos;
 struct exynos_pm_qos_request cluster1_qos;
 struct exynos_pm_qos_request mif_qos;
 struct exynos_pm_qos_request int_qos;
-struct exynos_pm_qos_request disp_qos;
 static bool ib_ufc_initialized;
 
 struct ufc_req ib_ufc_req;
@@ -56,12 +53,6 @@ void ib_set_booster(long *qos_values)
 			break;
 		case INT:
 			exynos_pm_qos_update_request(&int_qos, value);
-			/*
-			 * Touch-time INT boosts are synthesized in the generic
-			 * booster layer, so mirror that window into DISP to keep
-			 * the display fabric hot enough for 120 Hz UI animation.
-			 */
-			exynos_pm_qos_update_request(&disp_qos, TOUCH_DISP_BOOST_KHZ);
 			break;
 		default:
 			break;
@@ -98,8 +89,6 @@ void ib_release_booster(long *rel_flags)
 			break;
 		case INT:
 			exynos_pm_qos_update_request(&int_qos, release_val[INT]);
-			exynos_pm_qos_update_request(&disp_qos,
-				PM_QOS_DISPLAY_THROUGHPUT_DEFAULT_VALUE);
 			break;
 		default:
 			break;
@@ -144,8 +133,6 @@ int input_booster_init_vendor(void)
 		case INT:
 			exynos_pm_qos_add_request(&int_qos,
 				PM_QOS_DEVICE_THROUGHPUT, PM_QOS_DEVICE_THROUGHPUT_DEFAULT_VALUE);
-			exynos_pm_qos_add_request(&disp_qos,
-				PM_QOS_DISPLAY_THROUGHPUT, PM_QOS_DISPLAY_THROUGHPUT_DEFAULT_VALUE);
 			break;
 		default:
 			break;
@@ -175,7 +162,6 @@ void input_booster_exit_vendor(void)
 			break;
 		case INT:
 			exynos_pm_qos_remove_request(&int_qos);
-			exynos_pm_qos_remove_request(&disp_qos);
 			break;
 		default:
 			break;
